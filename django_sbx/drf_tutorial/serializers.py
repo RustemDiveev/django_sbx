74c817558db188c5e836cfb0457ef348dc386e52
Rustem.Diveev <rustem.diveev@glowbyteconsulting.com>
2022-05-12 18:06:24 +0300

    пример работы с сериализатором, из инстанса модели в байты и обратно

--- django_sbx/drf_tutorial/serializers.py
+++ django_sbx/drf_tutorial/serializers.py
@@ -23,5 +23,39 @@ class QSGroupSerializer(serializers.HyperlinkedModelSerializer):
 
     Объявление сериализаторов работает приблизительно также, как и объявление форм
 """
+class SnippetSerializer(serializers.Serializer):
+    
+    """
+        Сериализатор очень напоминает форму, и включает аналогичные флаги валидации для разных полей, такие как 
+        required, max_length и default
+    """
 
+    # Здесь определяются поля - который будут сериализованы / десериализованы 
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
+    # Как понял - это стиль формы - эквивалентно widgets=widgets.TextArea у Form 
+    code = serializers.CharField(style={"base_template": "textarea.html"})
+    linenos = serializers.BooleanField(required=False)
+    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default="python")
+    style = serializers.ChoiceField(choices=STYLE_CHOICES, default="friendly")
 
+    # create() и update() определяют, каким образом полноценные экземпляры модели будут сохранены при вызове serializer.save()
+
+    def create(self, validated_data):
+        """
+            Create and return a new Snippet instance, given the validated data
+        """
+        return Snippet.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+            Update and return an existing Snippet instance, given the validated data 
+        """
+        instance.title = validated_data.get("title", instance.title)
+        instance.code = validated_data.get("code", instance.code)
+        instance.linenos = validated_data.get("linenos", instance.linenos)
+        instance.language = validated_data.get("language", instance.language)
+        instance.style = validated_data.get("style", instance.style)
+        instance.save()
+
+        return instance 
