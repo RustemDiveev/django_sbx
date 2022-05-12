e3c967027dadc0f364516b913147b0166fbbe6b2
Rustem.Diveev <rustem.diveev@glowbyteconsulting.com>
2022-05-12 15:40:46 +0300

    Wrong fetch with main

---
+++ django_sbx/django_sbx/custom_settings.py
@@ -0,0 +1,12 @@
+SECRET_KEY = "!v*)-u*n*y7u(xth9c(o#tfb#-3!eip395!m$%n*iy9=c4f@5w"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "django_sbx",
+        "USER": "django_tech_user",
+        "PASSWORD": "EZheFinzl",
+        "HOST": "localhost",
+        "PORT": "5432"
+    },
+}
