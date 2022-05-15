475358686ba701256d981bc90383091edbc08757
Rustem Diveev <rustem.diveev@gmail.com>
2022-05-15 22:47:15 +0300

    pulled main branch

---
+++ django_sbx/django_sbx/custom_settings.py
@@ -0,0 +1,13 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-%+lz_x-9f2t2ik&v%@57ww@itp!zsc2fumj-hny*s0c#$#*0mx'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_tutorial',
+        'USER': 'django_tech_user',
+        'PASSWORD': 'Z20e7GkT7',
+        'HOST': 'localhost',
+        'PORT': '5432'
+    }
+}
