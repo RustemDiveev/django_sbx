4e3abf63a8cba604a2daaa33cf42e0841fe12577
Rustem Diveev <rustem.diveev@gmail.com>
2022-05-29 13:32:38 +0300

    info_text + старт проверки primary_key

--- django_sbx/models_tutorial/models.py
+++ django_sbx/models_tutorial/models.py
@@ -35,7 +35,28 @@ def default_various_callable_f():
 class DefaultModel(models.Model):
     """
         Для проверки default 
+        Можно передать как литерал, так и callable 
     """
     default = models.CharField(max_length=10, default="TEST")
     default_callable = models.CharField(max_length=10, default=default_callable_f)
     default_various_callable = models.IntegerField(default=default_various_callable_f)
+
+
+class HelpTextModel(models.Model):
+    """
+        Для проверки help_text - литерал, который будет доступен на виджете в форме + полезно для документации
+        Пока я просто создам модель и посмотрю, что видно в БД
+        В dbeaver - комментариев не видно 
+    """
+    char_column = models.CharField(help_text="Обычное строковое поле", max_length=100)
+    int_column = models.IntegerField(help_text="Обычное числовое поле")
+    date_column = models.DateField(help_text="Обычное поле дата")
+
+
+class PkModel(models.Model):
+    """
+        Для проверки primary key 
+    """
+    pk_column = models.CharField(max_length=100, primary_key=True)
+    char_column = models.CharField(max_length=100)
+    
