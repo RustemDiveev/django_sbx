d02464ffb76c93b13079d129b5d2ec3faf40fb1c
Rustem.Diveev <rustem.diveev@glowbyteconsulting.com>
2022-08-09 10:54:01 +0300

    новые пользователи для разделения по схемам

--- postgres_init_scripts/postprocess/init_new_schema.sql
+++ postgres_init_scripts/postprocess/init_new_schema.sql
@@ -1,3 +1,8 @@
 -- Выполнять под пользователем sbx_pp 
 CREATE SCHEMA catalog_one;
 CREATE SCHEMA catalog_two;
+
+-- Выполнять под пользователем postgres 
+CREATE USER catalog_one PASSWORD 'catalog_one';
+CREATE USER catalog_two PASSWORD 'catalog_two';
+
